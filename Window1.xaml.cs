88f92ebc92f7bd96aecc22545e827f185e1ae015
jakubekgranie-ekonom <kawkagaming@interia.pl>
2024-12-17 14:47:12 +0100

    Mon 17.12.2024 - GUI
    - See code.

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -19,9 +19,43 @@ namespace wpf_devourers
     /// </summary>
     public partial class Window1 : Window
     {
+        SolidColorBrush[][] toSwap = [[new(), new()], [new(), new()]];
+        Button[] senders = [new(), new()];
+        int i = -1;
+        bool pbSelection = false;
         public Window1()
         {
             InitializeComponent();
+        }
+        private void updateLabel(object sender, RoutedEventArgs e)
+        {
+            supportLabel.Content = "x " + dimensions.Text.ToString();
+        }
+        private void swapColor(object sender, RoutedEventArgs e) { 
+            if(((Button)sender).Name == "P1" || ((Button)sender).Name == "P2")
+                pbSelection = true;
+            if (i != 1 || (i == 0 && pbSelection)) // Prevent swapping unused colors
+            {
+                toSwap[++i] = [(SolidColorBrush)((Button)sender).Background, (SolidColorBrush)((Button)sender).BorderBrush];
+                senders[i] = (Button)sender;
+            }
+            if (i == 1)
+            {
+                if (!pbSelection)
+                {
+                    i = -1;
+                    return;
+                }
+                else
+                {
+                    senders[0].Background = toSwap[1][0];
+                    senders[0].BorderBrush = toSwap[1][1];
+                    senders[1].Background = toSwap[0][0];
+                    senders[1].BorderBrush = toSwap[0][1];
+                    i = -1;
+                    pbSelection = false;
+                }
+            }
         }
     }
 }
