d6a99e2adc2f3c5245bb11ff834e4fbb4fdceab1
jakubekgranie-ekonom <kawkagaming@interia.pl>
2024-12-18 14:46:02 +0100

    Wed 18.12.2024 - GUI 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,9 +18,11 @@ namespace wpf_devourers
         int[][] pucks = [[0, 0], [0, 0]];
         SolidColorBrush[][] Colors = [[new(), new()], [new(), new()], [new(Color.FromRgb(191, 237, 192)), new(Color.FromRgb(127, 219, 129))]];
         List<List<Button>> gameMatrix = [];
+        ResourceDictionary dictionary = new ResourceDictionary();
         public MainWindow()
         {
             InitializeComponent();
+            dictionary.Source = new Uri("pack://application:,,,/dict.xaml");
             Menu.ShowDialog();
             dimensions = Int32.Parse(Menu.dimensions.Text.ToString());
             if (dimensions > 20)
@@ -28,10 +30,14 @@ namespace wpf_devourers
             int sqrt = (int)Math.Ceiling(Math.Sqrt(dimensions));
             pucks[0][0] = pucks[1][0] = sqrt;
             pucks[0][1] = pucks[1][1] = sqrt / 2;
+            big_P1.Content = big_P2.Content = pucks[0][1];
+            medium_P1.Content = medium_P2.Content = pucks[0][0];
             Colors[0][0] = (SolidColorBrush)Menu.P1.Background;
             Colors[0][1] = (SolidColorBrush)Menu.P1.BorderBrush;
             Colors[1][0] = (SolidColorBrush)Menu.P2.Background;
             Colors[1][1] = (SolidColorBrush)Menu.P2.BorderBrush;
+            big_P1.Foreground = medium_P1.Foreground = Colors[0][1];
+            big_P2.Foreground = medium_P2.Foreground = Colors[1][1];
             double size = 110 / dimensions + 10;
             GameGridBorder.Width = GameGridBorder.Height = size * dimensions + 4;
             GameGridBorder.BorderBrush = Colors[2][1];
@@ -48,7 +54,7 @@ namespace wpf_devourers
                 };
                 GameGrid.ColumnDefinitions.Add(cdef);
             }
-            for(int i = 0;i < dimensions; i++)
+            for(int i = 0; i < dimensions; i++)
             {
                 gameMatrix.Add([]);
                 for(int j = 0; j < dimensions; j++)
@@ -59,7 +65,8 @@ namespace wpf_devourers
                         Background = Colors[2][0],
                         BorderBrush = Colors[2][1],
                         Width = Math.Ceiling(size),
-                        Height = Math.Ceiling(size)
+                        Height = Math.Ceiling(size),
+                        Style = dictionary["noHover"] as Style
                     };
                     GameGrid.Children.Add(Element);
                     Grid.SetColumn(Element, i);
@@ -67,6 +74,7 @@ namespace wpf_devourers
                     gameMatrix[i].Add(Element);
                 }
             }
+            turn.Foreground = Colors[0][0];
         }
     }
 }
