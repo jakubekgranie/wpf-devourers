cb2ee0d1545e677dfb9d6ec9d64f2cd8f7c2e187
jakubekgranie <jakubekgranie@60k>
2025-01-20 09:31:31 +0100

    Mon 20.01.2024 - Continued development
    See the code.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace wpf_devourers
             player = 0;
         String[] weightClasses = ["DBS", "DBM", "DBB"]; // weight classes
         int[][] pucks = [[0, 0], [0, 0]]; // pucks
-        int[] time = [0, 0]; // time
+        int[] time; // time
         Label[][] puckLabels = [[new(), new()], [new(), new()]];
         SolidColorBrush[][] Colors = [[new(), new()], [new(), new()], [new(Color.FromRgb(255, 218, 185)), new(Color.FromRgb(205, 133, 63))]]; // colors
         List<List<Button>> gameMatrix; // to change buttons respectively
@@ -25,10 +25,13 @@ namespace wpf_devourers
         double size = 110;
         double[] styleConstraints = [0.2, 0.35, 0.5];
         static System.Timers.Timer timeCounter = new System.Timers.Timer();
+        SolidColorBrush unselected = new SolidColorBrush(Color.FromRgb(0, 0 ,0)); 
         protected void createGrid()
         {
             gameMatrix = [];
             states = [];
+            time = [0, 0];
+            player = 0;
             int sqrt = (dimensions < (int)Math.Ceiling(Math.Sqrt(dimensions))) ? (int)Math.Ceiling(Math.Sqrt(dimensions)) : dimensions;
             size = 110 / dimensions + 10;
             GameGridBorder.Width = GameGridBorder.Height = size * dimensions + 4;
@@ -57,7 +60,8 @@ namespace wpf_devourers
                         Visibility = Visibility.Hidden,
                         Width = size * 0.3,
                         Height = size * 0.3,
-                        BorderThickness = new Thickness(2)
+                        BorderThickness = new Thickness(2),
+                        Background = unselected
                     },
                     Element = new()
                     {
@@ -81,6 +85,25 @@ namespace wpf_devourers
                 }
             }
         }
+        protected void updateLabels()
+        {
+            
+            big_P1.Foreground = medium_P1.Foreground = Colors[0][1];
+            big_P2.Foreground = medium_P2.Foreground = Colors[1][1];
+            turn.Foreground = Colors[player][0];
+            turn.Content = "P" + (player + 1);
+        }
+        protected void getPucks(int sqrt)
+        {
+            pucks[0][0] = pucks[1][0] = sqrt;
+            pucks[0][1] = pucks[1][1] = sqrt / 2;
+            big_P1.Content = big_P2.Content = pucks[0][1];
+            medium_P1.Content = medium_P2.Content = pucks[0][0];
+            puckLabels[0][0] = medium_P1;
+            puckLabels[0][1] = big_P1;
+            puckLabels[1][0] = medium_P2;
+            puckLabels[1][1] = big_P2;
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -91,26 +114,17 @@ namespace wpf_devourers
                 dimensions = 20;
             createGrid();
             int sqrt = (dimensions < (int)Math.Ceiling(Math.Sqrt(dimensions))) ? (int)Math.Ceiling(Math.Sqrt(dimensions)) : dimensions;
-            pucks[0][0] = pucks[1][0] = sqrt;
-            pucks[0][1] = pucks[1][1] = sqrt / 2;
-            big_P1.Content = big_P2.Content = pucks[0][1];
-            medium_P1.Content = medium_P2.Content = pucks[0][0];
-            puckLabels[0][0] = medium_P1;
-            puckLabels[0][1] = big_P1;
-            puckLabels[1][0] = medium_P2;
-            puckLabels[1][1] = big_P2;
+            getPucks(sqrt);
             Colors[0][0] = (SolidColorBrush)Menu.P1.Background;
             Colors[0][1] = (SolidColorBrush)Menu.P1.BorderBrush;
             Colors[1][0] = (SolidColorBrush)Menu.P2.Background;
             Colors[1][1] = (SolidColorBrush)Menu.P2.BorderBrush;
-            big_P1.Foreground = medium_P1.Foreground = Colors[0][1];
-            big_P2.Foreground = medium_P2.Foreground = Colors[1][1];
-            turn.Foreground = Colors[0][0];
+            updateLabels();
             timeCounter.Interval = 1000;
             timeCounter.Elapsed += trackTime;
             timeCounter.Enabled = true;
         }
-        protected void trackTime(object? sender, ElapsedEventArgs e)
+        protected void trackTime(object? sender, ElapsedEventArgs? e)
         {
             Dispatcher.Invoke(() =>
             {
@@ -132,6 +146,13 @@ namespace wpf_devourers
             target.BorderBrush = Colors[player][1];
             target.Style = dictionary[weightClasses[weight] + "-rotation"] as Style;
         }
+        protected void win()
+        {
+            timeCounter.Enabled = false;
+            createGrid();
+            getPucks((dimensions < (int)Math.Ceiling(Math.Sqrt(dimensions))) ? (int)Math.Ceiling(Math.Sqrt(dimensions)) : dimensions);
+            updateLabels();
+        }
         protected void changeElementStance(object sender, RoutedEventArgs e)
         {
             int[] coords = ((Button)sender).Tag as int[];
@@ -148,6 +169,48 @@ namespace wpf_devourers
                 player = (player + 1) % 2;
                 turn.Foreground = Colors[player][1];
                 turn.Content = "P" + (player + 1);
+            }
+            for (int i = 0; i < dimensions; i++)
+            {
+                Color background = ((SolidColorBrush)gameMatrix[i][0].Background).Color;
+                if (background == unselected.Color)
+                    return;
+                bool flag = true;
+                for (int j = 1; j < dimensions; j++)
+                    if(((SolidColorBrush)gameMatrix[i][0].Background).Color != background)
+                    {
+                        flag = false;
+                        break;
+                    }
+                if (flag)
+                {
+                    win();
+                    return;
+                }
+            }
+            Color background1 = ((SolidColorBrush)gameMatrix[0][0].Background).Color;
+            if (background1 == unselected.Color)
+                return;
+            bool flag2 = true;
+            for (int i = 1; i < dimensions; i++)
+                if (((SolidColorBrush)gameMatrix[i][i].Background).Color != background1)
+                    flag2 = false;
+            if (flag2)
+            {
+                win();
+                return;
+            }
+            flag2 = true;
+            background1 = ((SolidColorBrush)gameMatrix[gameMatrix.Count - 1][0].Background).Color;
+            if (background1 == unselected.Color)
+                return;
+            for (int i = 1; i > dimensions; i++)
+                if (((SolidColorBrush)gameMatrix[gameMatrix.Count - 1 - i][i].Background).Color != background1)
+                    flag2 = false;
+            if (flag2)
+            {
+                win();
+                return;
             }
         }
         protected void changePreviewButton(int type = 0)
@@ -184,11 +247,10 @@ namespace wpf_devourers
                         Color color = Colors[i][j].Color;
                         toFile += $"{color.R} {color.G} {color.B} ";
                     }
-                int a = states.Count;
                 toFile += "\n";
                 for (int i = 0; i < states.Count; i++)
                 {
-                    for (int j = 0; j < states[0].Count; j++)
+                    for (int j = 0; j < states[i].Count; j++)
                     {
                         if (states[i][j] == -1)
                             toFile += "3 ";
@@ -211,7 +273,10 @@ namespace wpf_devourers
                     }
                     toFile += "\n";
                 }
-                toFile += $"{time[0]} {time[1]}\n{player}";
+                toFile += $"{time[0]} {time[1]}\n{player}\n";
+                for (int i = 0; i < 2; i++)
+                    for (int j = 0; j < 2; j++)
+                        toFile += $"{pucks[i][j] }";
                 File.WriteAllText(file.FileName, toFile);
             }
         }
@@ -226,8 +291,8 @@ namespace wpf_devourers
             bool? result = file.ShowDialog();
             if (result == true)
             {
-                //try
-                //{
+                try
+                {
                     timeCounter.Enabled = false;
                     String[] fromFile = File.ReadAllLines(file.FileName);
                     dimensions = Int32.Parse(fromFile[0]);
@@ -247,20 +312,6 @@ namespace wpf_devourers
                         states.Add([]);
                         tempArray = fromFile[i + 2].Split(" ");
                         for (int j = 0; j < tempArray.Length; j++)
-                            if (tempArray[j] != "")
-                            {
-                                int value = Int32.Parse(tempArray[j]);
-                                if (value == 3)
-                                    states[i].Add(-1);
-                                else
-                                    states[i].Add(value);
-                            }
-                    }
-                    for (int i = 0; i < dimensions; i++)
-                    {
-                        states.Add([]);
-                        tempArray = fromFile[i + 2].Split(" ");
-                        for (int j = 0; j < tempArray.Length; j += 2)
                             if (tempArray[j] != "")
                             {
                                 int value = Int32.Parse(tempArray[j]);
@@ -304,15 +355,32 @@ namespace wpf_devourers
                                 elementStyleAdjustment(ElementRepresentative, states[i][j]);
                             gameMatrix[i].Add(ElementRepresentative);
                         }
-                        tempArray = fromFile[2 + dimensions * 2].Split(' ');
-                        time = [Int32.Parse(tempArray[0]), Int32.Parse(tempArray[1])];
-                        timeCounter.Enabled = true;
-                        player = Int32.Parse(fromFile[3 + dimensions * 2]);
+                        
                     }
-                //}
-                //catch(Exception ex) {
-                    //MessageBox.Show($"An unexpected error occured:\n\n{ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                //}
+                    for (int i = 0; i < dimensions; i++)
+                    {
+                        tempArray = fromFile[i + 2 + dimensions].Split(' ');
+                        for (int j = 0; j < tempArray.Length; j++)
+                            if (tempArray[j] != "2" && tempArray[j] != "")
+                            {
+                                gameMatrix[i][j].Background = Colors[Int32.Parse(tempArray[j])][0];
+                                gameMatrix[i][j].BorderBrush = Colors[Int32.Parse(tempArray[j])][1];
+                            }
+                    }
+                    tempArray = fromFile[2 + dimensions * 2].Split(' ');
+                    time = [Int32.Parse(tempArray[0]), Int32.Parse(tempArray[1])];
+                    player = Int32.Parse(fromFile[3 + dimensions * 2]);
+                    tempArray = fromFile[4 + dimensions * 2].Split(' ');
+                    for (int i = 0; i < tempArray.Length - 1; i++)
+                        pucks[i / 3][i % 2] = Int32.Parse(tempArray[i]);
+                    updateLabels();
+                    time[1]--;
+                    trackTime(null, null);
+                    timeCounter.Enabled = true;
+                }
+                catch(Exception ex) {
+                    MessageBox.Show($"An unexpected error occured:\n\n{ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
         }
     }
